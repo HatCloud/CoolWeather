45bab8b988b902ac350ecff5276eb11a67de0555
Hat_Cloud <jijianfeng529@gmail.com>
2015-04-08 14:50:04 +0800

    加入显示天气信息的功能，因为中国天气提供的API发生改变，所以改变了处理服务器返回信息的逻辑

--- app/src/main/java/com/jeff/coolweather/util/Utility.java
+++ app/src/main/java/com/jeff/coolweather/util/Utility.java
@@ -1,11 +1,22 @@
 package com.jeff.coolweather.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.jeff.coolweather.db.CoolWeatherDB;
 import com.jeff.coolweather.model.City;
 import com.jeff.coolweather.model.County;
 import com.jeff.coolweather.model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Jeff on 15/4/7.
@@ -74,5 +85,44 @@ public class Utility {
         return false;
     }
 
+    /**
+     * 解析服务器返回的JSON数据，并将解析出的数据存储到本地
+     * @param context
+     * @param response
+     */
+    public static void handleWeatherResponse(Context context, String response) {
+        Log.d("WeatherActivity", "Response:" + response);
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            Log.d("WeatherActivity", "jsonObject");
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            Log.d("WeatherActivity", "weatherInfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            //String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather1");
+            String publishTime = weatherInfo.getString("date_y");
+            Log.d("WeatherActivity", "weatherDesp：" + weatherDesp);
+            saveWeatherInfo(context, cityName, weatherCode, temp1, weatherDesp, publishTime);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveWeatherInfo(Context context, String cityName, String weatherCode,
+                                       String temp1, String weatherDesp, String publishTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        //editor.putString("temp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date", sdf.format(new Date()));
+        editor.commit();
+    }
 
 }
